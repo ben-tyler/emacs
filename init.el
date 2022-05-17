1cb35c62303c856be3b67e611772cdc4ff2c3205
ben-tyler <105482041+ben-tyler@users.noreply.github.com>
2022-05-18 07:40:01 +1000

    Update init.el
    add in undo tree for evil mode

--- init.el
+++ init.el
@@ -155,3 +155,27 @@
   )
 
 (setq-default typescript-indent-level 2)
+(load-theme 'doom-flatwhite)
+
+
+(use-package undo-tree
+  :ensure t
+  :after evil
+  :diminish
+  :config
+  (evil-set-undo-system 'undo-tree)
+  (global-undo-tree-mode 1))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3d54650e34fa27561eb81fc3ceed504970cc553cfd37f46e8a80ec32254a3ec3" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
